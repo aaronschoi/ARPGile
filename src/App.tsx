3985f81b50a3cb01ccf5ff4a4e03f51f871d6245
Aaron Choi <connect@aaronschoi.com>
2022-10-05 22:34:44 -0700

    setup test and sass module

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Route, Routes } from "@solidjs/router";
 import type { Component } from "solid-js";
-import styles from "./App.module.css";
+import styles from "./App.module.scss";
 
 const App: Component = () => {
   return (
+    <>
+    <h1 class={styles.title}>ARGPile</h1>
     <Routes>
       <Route path="/" element={<div>First Page of ARGPile</div>} />
     </Routes>
+    </>
   );
 };
 
